68ea8fda29dbc8fadad068051ec0dc2e5686d96e
TENMAJKL <tenmajkl@liskyjidlokod.fun>
2022-05-03 16:35:49 +0200

    finishes hs solution

--- 03/TENMAJKL.hs
+++ 03/TENMAJKL.hs
@@ -1,10 +1,7 @@
--- TODO unfinished
-
-import Data.List (isSubsequenceOf, permutations)
-
+isColorChar :: Char -> Bool
 isColorChar char = char `elem` (['a'..'f'] ++ ['0'..'9'])
 
 isColor :: String -> Bool
-isColor target = (head target == '#') && (length (tail target) == 6) && filter isColorChar (tail target)
+isColor target = (head target == '#') && (length (tail target) == 6) && length (filter isColorChar (tail target)) == 6
 
-main = print( isColor "#ffff00")
+main = print( isColor "#ffx00f")
