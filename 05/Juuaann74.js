7beb37748514b649a9d05f3273560d6c0a08d9ed
Vilém Haupt <71026000+vilemhaupt@users.noreply.github.com>
2022-05-17 20:24:47 +0200

    Create Juuaann74.js

---
+++ 05/Juuaann74.js
@@ -0,0 +1,15 @@
+function arrayMax(num1,num2){
+
+    const newArr = [];
+
+    for (i = num1; i < num2+1; i++) {
+        newArr.push(i);
+    }
+
+    console.log(newArr);
+
+
+
+}
+
+arrayMax(1,5);
