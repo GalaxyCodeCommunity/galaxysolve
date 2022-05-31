61573d93c6a7f35ba48e7898c8a02864e48b6df1
TENMAJKL <tenmajkl@liskyjidlokod.fun>
2022-05-31 22:40:59 +0200

    fixes hs

--- 07/TENMAJKL.hs
+++ 07/TENMAJKL.hs
@@ -1,12 +1,7 @@
--- awfull
-
-smallest :: [Int] -> Int
-smallest items = foldr (\ x y -> if x < y then x else y) (head items) items
-
 count :: String -> [Int]
-count = foldr (\ x y -> if x == '(' then (head y + 1) : tail y else if x == ')' then head y : [head (tail y) + 1] else y) [0, 0] 
+count = foldl (\ y x -> if x == '(' then (head y + 1) : tail y else if x == ')' && head y > 0 then head y - 1 : [y !! 1 + 1] else y) [0, 0] -- chce se mi zvracet - zdenda polreich
 
-countBrackets :: String  -> Int 
-countBrackets target = smallest (count target)
+countBrackets :: String -> Int 
+countBrackets target = count target !! 1
 
-main = print (countBrackets "(parek(rizek))(rizek)")
+main = print (countBrackets ")((cs)cs))")
