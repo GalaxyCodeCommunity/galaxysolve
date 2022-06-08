fb96bcef985fb34a04f88f59e0268e4a3fd2c056
TENMAJKL <tenmajkl@liskyjidlokod.fun>
2022-06-08 21:55:54 +0200

    doesnt work :|

--- 08/TENMAJKL.php
+++ 08/TENMAJKL.php
@@ -1,9 +1,12 @@
 <?php
 
-function fmap(int $x, int $a, int $b, int $c, int $d): float
+// TODO
+
+function fmap(float $x, float $a, float $b, float $c, float $d): float
 {
     $shift = $a - $c;
     $b_shifted = $b - $shift;
     $mult = $d / $b_shifted;
     return ($x - $shift) * $mult;
 }
+
