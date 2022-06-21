1220d704d9a65c7e9b55ee2d1a8bb4f722314331
TENMAJKL <tenmajkl@liskyjidlokod.fun>
2022-06-21 15:09:42 +0200

    adds working 08

--- 08/TENMAJKL.php
+++ 08/TENMAJKL.php
@@ -1,12 +1,6 @@
 <?php
 
-// TODO
-
 function fmap(float $x, float $a, float $b, float $c, float $d): float
 {
-    $shift = $a - $c;
-    $b_shifted = $b - $shift;
-    $mult = $d / $b_shifted;
-    return ($x - $shift) * $mult;
+    return ($d - $c) / ($b - $a) * ($x - $a) + $c;
 }
-
