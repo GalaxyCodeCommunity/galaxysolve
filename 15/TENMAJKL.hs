b42bee981613a372ec6f6b6b3037fdad0d51b757
TENMAJKL <tenmajkl@liskyjidlokod.fun>
2022-07-27 15:24:19 +0200

    optimalization

--- 15/TENMAJKL.hs
+++ 15/TENMAJKL.hs
@@ -1,12 +1,19 @@
 import Data.Char (ord)
 
+dividers :: Int -> [Int]
+dividers number = filter (\item -> number `mod` item == 0) [1..number]
+
+findBest :: Int -> Int
+findBest number = foldl (\cary item -> if (item + (number `div` item)) < (cary + (number `div` cary)) then item else cary) number (dividers number)
+
+generateCode :: Int -> Char -> String
+generateCode number operation = ">" ++ replicate (findBest number) '+' ++ "[<" ++ replicate (number `div` findBest number) operation ++ ">-]<." 
+
 convertChar :: Char -> Int -> String
 convertChar target last = 
-    (
-        if ord target - last > 0 
-        then replicate (ord target - last) '+' 
-        else replicate (last - ord target) '-'
-    ) ++ "."
+    if ord target - last > 0 
+    then generateCode (ord target - last) '+' 
+    else generateCode (last - ord target) '-'
 
 convert :: String -> Int -> String
 convert target last 
